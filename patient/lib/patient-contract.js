6dd5db776289dbc796d0c1423ed709d319aec576
ezrafrost7 <71141893+ezrafrost7@users.noreply.github.com>
2022-01-13 23:48:27 -0700

    added an init for the patient smart contract and edited the pacakage.json file. init function does not yet work

--- patient/lib/patient-contract.js
+++ patient/lib/patient-contract.js
@@ -6,9 +6,30 @@
 'use strict';
 
 const { Contract } = require('fabric-contract-api');
+const stringify  = require('json-stringify-deterministic');
 
 class PatientContract extends Contract {
     //patient smart contract object 
 
+    async InitPatient(context) {
+        //initiate the patient's ledger
+        var today = new Date()
+
+        const data = [
+            {
+                ProviderID: 'admin',
+                VisitDate: today.getDate().toString(),
+                Description: 'initiation of patient data',
+                Notes: 'initiation of healthcare ledger'
+            }
+        ];
+
+        for (const datum of data) {
+            datum.docType = 'asset';
+            await context.stub.putState(datum.ProviderID, Buffer.from(stringify(sortKeysRecursive(datum))))
+        }
+    }
 
 }
+
+module.exports = PatientContract;
