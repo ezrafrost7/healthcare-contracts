afa6ae91a893f6a5a560770f3f5cfdf0f957fba1
ezrafrost7 <71141893+ezrafrost7@users.noreply.github.com>
2022-01-24 18:56:44 -0700

    admin smartcontracts don't work, register but dont function

--- admin/lib/admin-contract.js
+++ admin/lib/admin-contract.js
@@ -8,6 +8,8 @@
 const {Contract} = require('fabric-contract-api');
 const stringify  = require('json-stringify-deterministic');
 const sortKeysRecursive  = require('sort-keys-recursive');
+const fs = require('fs');
+const {exec} = require('child_process');
 
 //Standard class, instantiation for creation of smart contract
 class AdminContract extends Contract {
@@ -16,7 +18,34 @@ class AdminContract extends Contract {
     async InitLedger(ctx) {
         //initiate the admin ledger
 
+        await ctx.stub.putState("init", "Admin ledger initiated")
 
+    }
+
+    async CreateChannel(ctx, CHANNEL_NAME) {
+        //creating a patient channel
+
+        const channelArtifacts = './channelArtifacts';
+
+        if (!fs.existsSync(channelArtifacts)) {
+            fs.mkdirSync(channelArtifacts)
+        }
+
+        exec(`configtxgen -profile TwoOrgsApplicationGenesis -outputBlock ./channel-artifacts/${CHANNEL_NAME}.block -channelID ${CHANNEL_NAME}`, (err,stdout,stderr) => {
+
+        if (err) {
+            console.error(err);
+            return;
+        } if (stderr) {
+            console.error(stderr);
+            return;
+        }
+
+        console.log(stdout);
+        //await ctx.stub.putState(stringify(stdout), Buffer.from(stringify(stdout)))
+        return JSON.stringify(stdout);
+
+    });
 
     }
 
